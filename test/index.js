638efb62d1e428290f6ffdd6b62cd76134058b47
Dexter <me@dextermiguel.com>
2019-06-18 11:47:41 -0700

    Added semi-colons

--- test/index.js
+++ test/index.js
@@ -2,13 +2,13 @@
  * Module tests
  */
 
-var should = require('should')
+var should = require('should');
 
 describe('module', function () {
   it('should expose Router', function (done) {
-    var Router = require('..')
-    should.exist(Router)
-    Router.should.be.type('function')
-    done()
-  })
-})
+    var Router = require('..');
+    should.exist(Router);
+    Router.should.be.type('function');
+    done();
+  });
+});
