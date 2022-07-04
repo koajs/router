6aca7205bf6914bcb6bd1b0af5316d9bf8f57d51
Peter Vermeulen <nmb.peterv@gmail.com>
2022-07-04 08:26:17 +0200

    Improve path checking before route registration (#155)
    * [147] Ensure path sanity checks provide clear errors during registration checks
    
    * [147] Adjust tests to not include unused variables
    
    * [147] Adjust usage of double quotes to single quotes to ensure code style
    
    * [147] Adjust gh-148 to gh-147 mention in tests
    
    * [147] Remove extraneous parameters in test functions, ensure all possible method verbs are tested

--- test/lib/router.js
+++ test/lib/router.js
@@ -993,7 +993,26 @@ describe('Router', function () {
           done();
         });
     });
+    
+    it ('correctly returns an error when not passed a path for verb-specific registration (gh-147)', function () {
+      const router = new Router();
+      for (let el of methods) {
+          try {
+            router[el](function () {});
+          } catch (e) {
+            expect(e.message).to.be(`You have to provide a path when adding a ${el} handler`);
+          }
+      }
+    });
 
+    it ('correctly returns an error when not passed a path for "all" registration (gh-147)', function () {
+      const router = new Router();
+      try {
+        router.all(function () {});
+      } catch (e) {
+        expect(e.message).to.be('You have to provide a path when adding an all handler');
+      }
+    });
   });
 
   describe('Router#use()', function () {
