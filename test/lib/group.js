5328cf8d03b4b16d9a1a8d2e1e43b2c009472bb1
Leonardo Silveira <sombriks@gmail.com>
2023-08-22 00:42:15 -0300

    feat: works on #166

--- test/lib/group.js
+++ test/lib/group.js
@@ -1,9 +1,32 @@
 /**
  * Group tests
  */
+const expect = require('expect.js');
+const { Group } = require('../../lib/group');
+const Router = require('../../lib/router');
 
 describe('Group', function () {
-  it('Should build a group', function (done) {
-    done();
+  it('Should build a router from a group', function (done) {
+    try {
+      const group = new Group();
+      expect(group).to.be.ok();
+      const router = group.build();
+      expect(router).to.be.ok();
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
+
+  it('Should build a router from a group using alias under Router.Group', function (done) {
+    try {
+      const group = new Router.Group();
+      expect(group).to.be.ok();
+      const router = group.build();
+      expect(router).to.be.ok();
+      done();
+    } catch (err) {
+      done(err);
+    }
   });
 });
