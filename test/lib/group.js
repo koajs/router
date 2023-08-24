ce69849eebaba303b31a01d6a4964e50cf43e8d6
Leonardo Silveira <sombriks@gmail.com>
2023-08-24 00:40:40 -0300

    feat(#166): adding callback-style building

--- test/lib/group.js
+++ test/lib/group.js
@@ -52,4 +52,76 @@ describe('Group', function () {
     expect(router.stack[0].stack[0]).to.eql(foo);
     done();
   });
+
+  it('Should build a router with a single path and a get verb, callback style', function (done) {
+    function foo() {}
+
+    const group = new Router.Group().path('/hello', (group) => {
+      group.get(foo);
+    });
+    const router = group.build();
+
+    expect(router).to.be.ok();
+    expect(router.stack[0].path).to.eql('/hello');
+    expect(router.stack[0].stack[0]).to.eql(foo);
+    done();
+  });
+
+  it('should nest paths correctly', function (done) {
+    function list() {}
+    function find() {}
+    function create() {}
+    function update() {}
+    function del() {}
+
+    const group = new Group().path('/users', (group) => {
+      group.get(list);
+      group.post(create);
+      group.path('/:id', (group) => {
+        group.get(find);
+        group.put(update);
+        group.delete(del);
+      });
+    });
+
+    const router = group.build();
+
+    expect(router).to.be.ok();
+    expect(router.stack.map((layer) => layer.path)).to.contain('/users');
+    expect(router.stack.map((layer) => layer.path)).to.contain('/users/:id');
+
+    expect(
+      router.stack
+        .filter((layer) => layer.path === '/users')
+        .map((layer) => layer.stack)
+        .flat(Number.POSITIVE_INFINITY)
+    ).to.contain(list);
+    expect(
+      router.stack
+        .filter((layer) => layer.path === '/users')
+        .map((layer) => layer.stack)
+        .flat(Number.POSITIVE_INFINITY)
+    ).to.contain(create);
+
+    expect(
+      router.stack
+        .filter((layer) => layer.path === '/users/:id')
+        .map((layer) => layer.stack)
+        .flat(Number.POSITIVE_INFINITY)
+    ).to.contain(find);
+    expect(
+      router.stack
+        .filter((layer) => layer.path === '/users/:id')
+        .map((layer) => layer.stack)
+        .flat(Number.POSITIVE_INFINITY)
+    ).to.contain(update);
+    expect(
+      router.stack
+        .filter((layer) => layer.path === '/users/:id')
+        .map((layer) => layer.stack)
+        .flat(Number.POSITIVE_INFINITY)
+    ).to.contain(del);
+
+    done();
+  });
 });
